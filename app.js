7cad0fe306cb6d26e7fe16c558b4535d9bb4fcd5
abdulrahmanabdullah <nfs056@gmail.com>
2019-09-28 14:51:53 +0300

    adding unorderd list

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// Get main view 
+// main UI contnet 
+const contentUI = document.querySelector('.list-content');
+const listUI = document.querySelector('.list');
+
+const createItems = (list) => {
+    // temporary element 
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i <= 8; i++) {
+        let li = document.createElement('li');
+        li.className = 'item';
+        fragment.appendChild(li);
+    }
+    list.appendChild(fragment);
+}
+createItems(listUI);
+
+// delegation event
+listUI.addEventListener('click', (e) => {
+    // check before execut . 
+    if (!e.target.matches('li')) return;
+    // To avoid to change background color that already has. 
+    if (e.target.classList.contains('added')) return;
+    changeColor(e.target);
+});
+
+// random color 
+function randomColor(number) {
+    return Math.floor(Math.random() * number);
+}
+
+// change bg color 
+function changeColor(target) {
+    let color = `rgb( ${randomColor(200)},${randomColor(250)},${randomColor(250)})`;
+    target.classList.add('added');
+    target.style.backgroundColor = color;
+}
